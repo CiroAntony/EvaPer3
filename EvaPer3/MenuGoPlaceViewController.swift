0818fc197f4dddf60ad81e9d60bcdd05573a02c0
CiroAntony <85654533+CiroAntony@users.noreply.github.com>
2021-12-08 19:38:41 -0500

    se creo un menu con el cual se podrá acceder al resto de opciones

--- EvaPer3/MenuGoPlaceViewController.swift
+++ EvaPer3/MenuGoPlaceViewController.swift
@@ -11,10 +11,26 @@ import FirebaseAuth
 
 class MenuGoPlaceViewController: UIViewController {
 
+    @IBOutlet weak var solicitarButton: UIButton!
+    
+    @IBOutlet weak var pendientesButton: UIButton!
+    
+    @IBOutlet weak var aprobadosButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        solicitarButton.backgroundColor = UIColor.init(red: 48/255, green: 173/255, blue: 99/255, alpha: 1)
+        solicitarButton.layer.cornerRadius = 25.0
+        solicitarButton.tintColor = UIColor.white
+        
+        pendientesButton.backgroundColor = UIColor.init(red: 48/255, green: 173/255, blue: 99/255, alpha: 1)
+        pendientesButton.layer.cornerRadius = 25.0
+        pendientesButton.tintColor = UIColor.white
+        
+        aprobadosButton.backgroundColor = UIColor.init(red: 48/255, green: 173/255, blue: 99/255, alpha: 1)
+        aprobadosButton.layer.cornerRadius = 25.0
+        aprobadosButton.tintColor = UIColor.white
+        
     }
-    
-
 }
